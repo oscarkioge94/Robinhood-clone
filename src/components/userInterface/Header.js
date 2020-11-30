cd387351f955d742ee34a47aaf12fd25fac0968d
oscar Kioge <mainoscar94@gmail.com>
2020-11-30 11:42:12 +0300

    done with the header component

--- src/components/userInterface/Header.js
+++ src/components/userInterface/Header.js
@@ -12,8 +12,17 @@ export default function Header() {
         {/* search */}
         <div className="header__search">
             <div className="header__searchContainer">
-                
+                <input placeholder="Search" type="text" />
             </div>
+        </div>
+        {/* menuitems */}
+        <div className="header__menuItems">
+            <a href="#">Free Stocks</a>
+            <a href="#">Portfolio</a>
+            <a href="#">Cash</a>
+            <a href="#">Messages</a>
+            <a href="#">Account</a>
+
         </div>
 
         </div>
